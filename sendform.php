6dc186a89a5510a5db7af917a6494b50019588bb
Arbass <13ar.bass@gmail.com>
2018-04-21 01:14:14 +0300

    Форма ВК

---
+++ sendform.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Отправление, пожалуйста подождите...</title>
+	<meta http-equiv="refresh" content="3; url=/"> <!-- Редирект на главную страницу -->
+</head>
+<body>
+	<div class="loader">
+		<div class="center">
+			<h1 style="text-align: center;">С Вами свяжутся в скором времени. Спасибо!</h1>
+		</div>
+	</div>
+</body>
+</html>
+
+<?php
+ 	$name = $_POST['name']; // input name
+ 	$phone = $_POST['phone']; // input phone
+ 	$email = $_POST['email']; // input phone
+  $city = $_POST['city']; // input phone
+
+	$message = "Новый заказ на сайте".PHP_EOL."Имя: ".$name.PHP_EOL."Телефон: ".$phone.PHP_EOL."Email: ".$email.PHP_EOL."Город: ".$city;
+
+	send(412301537,$message); // id беседы с заказчиком
+
+	function send($id , $message) {
+    $url = 'https://api.vk.com/method/messages.send';
+    $params = array(
+      'user_id' => $id,    // Кому отправляем
+      'message' => $message,   // Что отправляем
+      'access_token' => '2a8036e0e697a935cece291b9129c62636d0a537bb42543d93534d5fda9761d11d29c0d50ddeafb8f8235',  
+      'v' => '5.62',
+    );
+
+    $result = file_get_contents($url, false, stream_context_create(array(
+        'http' => array(
+          'method'  => 'POST',
+          'header'  => 'Content-type: application/x-www-form-urlencoded',
+          'content' => http_build_query($params)
+        )
+    )));
+	}
+?>
